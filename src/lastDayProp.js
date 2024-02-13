0a8f8938716e4a3e7fdcb371917dce7f11038ddc
Hayk Khachatryan <hi@hayk.io>
2024-02-12 19:02:48 -0500

    fix(lastDayProp): better export

--- src/lastDayProp.js
+++ src/lastDayProp.js
@@ -1,36 +1,28 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const findLastDayString = (tp, lastNumber = 1) => __awaiter(void 0, void 0, void 0, function* () {
+exports.getPropFromLastDay = void 0;
+const findLastDayString = async (tp, lastNumber = 1) => {
     // debugger
     const dayString = `journals/days/${tp.date.now("YYYY-MM-DD ddd", -lastNumber)}.md`;
-    const file = yield tp.file.exists(dayString);
+    const file = await tp.file.exists(dayString);
     if (file)
         return dayString;
     else if (lastNumber > 50)
         return null;
     else
         return findLastDayString(tp, lastNumber + 1);
-});
-const findLastDayTFile = (tp) => __awaiter(void 0, void 0, void 0, function* () {
+};
+const findLastDayTFile = async (tp) => {
     // debugger
-    const lastDayString = yield findLastDayString(tp);
+    const lastDayString = await findLastDayString(tp);
     if (lastDayString)
         return tp.file.find_tfile(lastDayString);
     return null;
-});
-const getPropFromTFile = (tp, tFile, prop) => __awaiter(void 0, void 0, void 0, function* () {
+};
+const getPropFromTFile = async (tp, tFile, prop) => {
     // debugger
     const re = new RegExp(`${prop}: (?<value>.+)`);
-    const file = yield tp.file.include(tFile);
+    const file = await tp.file.include(tFile);
     if (file) {
         const prop = re.exec(file);
         if ((prop === null || prop === void 0 ? void 0 : prop.groups) && "value" in prop.groups) {
@@ -38,11 +30,11 @@ const getPropFromTFile = (tp, tFile, prop) => __awaiter(void 0, void 0, void 0, 
         }
     }
     return null;
-});
-const getPropFromLastDay = (tp, prop) => __awaiter(void 0, void 0, void 0, function* () {
+};
+const getPropFromLastDay = async (tp, prop) => {
     // debugger;
-    const lastDayFile = yield findLastDayTFile(tp);
+    const lastDayFile = await findLastDayTFile(tp);
     if (lastDayFile)
-        return yield getPropFromTFile(tp, lastDayFile, prop);
-});
-module.exports = getPropFromLastDay;
+        return await getPropFromTFile(tp, lastDayFile, prop);
+};
+exports.getPropFromLastDay = getPropFromLastDay;
